9f971751eaaa971f0e4a93a20f55fc8e4f2c3818
vanusha <ivan.korotchenya@gmail.com>
2022-11-11 23:05:33 +0300

    0.1

--- db_config.py
+++ db_config.py
@@ -1,4 +1,4 @@
 host = '127.0.0.1'
 user = 'postgres'
-password = 'qwerty'
+password = '25692569'
 db_name = "requests"
--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 from flask import Flask, request, render_template
 import psycopg2
 from db_config import host, user, password, db_name
+import json
 
 app = Flask(__name__)
+
+insert_query = """
+INSERT INTO request (headers, method, initiator, url, timestamp, type, document, frame, request) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);
+"""
 
 
 @app.route("/extinsion_api", methods=["POST"])
 def new_record():
     if request.method == "POST":
+        data = json.loads(request.data)
         with psycopg2.connect(host=host, user=user, password=password, database=db_name) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM table")
-
-        print(request.data.decode())
+                cursor.execute(insert_query, [json.dumps(data.get(name)) if data.get(name, None) else None for name in
+                                                                                      ['headers', 'method', 'initiator', 'url', 'timestamp', 'type', 'document', 'frame', 'request']
+                                                                                      ])
         return '', 200
 
 
@@ -21,7 +27,15 @@ def extinsion():
     if request.method == "GET":
         return render_template('extinsions.html', context={})
     elif request.method == "POST":
-        return render_template()
+        query = request.form['text']
+        with psycopg2.connect(host=host, user=user, password=password, database=db_name) as conn:
+            with conn.cursor() as cursor:
+                try:
+                    cursor.execute(query)
+                    data = cursor.fetchall()
+                except:
+                    data = 'Error'
+        return f'{data}'
 
 
 if __name__ == "__main__":
